bfb478138e47fa09e15a258b4508f86cf2f98632
VN Phạm <nam.pv1805@gmail.com>
2025-11-06 22:44:02 +0100

    update AllocInfo

--- internal/info/info.go
+++ internal/info/info.go
@@ -5,11 +5,18 @@ import (
     "encoding/binary"
 )
 
+type AllocInfoRaw struct {
+	Size       	uint64
+	TimestampNs uint64
+	StackId    	uint32
+	_ 			[4]byte
+}
+
 type AllocInfo struct {
 	Size       	uint64
 	TimestampNs uint64
-	StackId    	int32
-	_ 			[4]byte
+	StackId    	uint32
+	Address 	uint64
 }
 
 type CombinedAllocInfoRaw struct {
@@ -17,21 +24,30 @@ type CombinedAllocInfoRaw struct {
 }
 
 type CombinedAllocInfo struct {
-	TotalSize      uint64
-	NumberOfAllocs uint32
+	StackId      	uint32
+	TotalSize      	uint64
+	NumberOfAllocs 	uint32
 }
 
-func UnmarshalBinaryAllocInfo(data []byte) (*AllocInfo, error) {
-	var allocInfo AllocInfo
+func UnmarshalBinaryAllocInfo(data []byte) (*AllocInfoRaw, error) {
+	var allocInfoRaw AllocInfoRaw
     reader := bytes.NewReader(data)
-    if err := binary.Read(reader, binary.LittleEndian, &allocInfo); err != nil {
+    if err := binary.Read(reader, binary.LittleEndian, &allocInfoRaw); err != nil {
         return nil, err
     }
-    return &allocInfo, nil
+    return &allocInfoRaw, nil
 }
 
+func GetAllocInfo(address uint64, raw AllocInfoRaw) AllocInfo {
+	return AllocInfo{
+		Size:        raw.Size,
+		TimestampNs: raw.TimestampNs,
+		StackId:     raw.StackId,
+		Address:     address,
+	}
+}
 
-func GetCombinedAllocInfo(raw CombinedAllocInfoRaw) (*CombinedAllocInfo, error) {
+func GetCombinedAllocInfo(stackId uint32, raw CombinedAllocInfoRaw) (*CombinedAllocInfo, error) {
     total := uint64(raw.Bits[0]) |
         uint64(raw.Bits[1])<<8 |
         uint64(raw.Bits[2])<<16 |
@@ -43,6 +59,7 @@ func GetCombinedAllocInfo(raw CombinedAllocInfoRaw) (*CombinedAllocInfo, error) 
         uint32(raw.Bits[7])<<16
 
     return &CombinedAllocInfo{
+		StackId:        stackId,
         TotalSize:      total,
         NumberOfAllocs: number,
     }, nil
